9dbfad90837e64f1c763aa1368b047e405834bc7
unknown <shustart@gmail.com>
2020-10-06 23:39:01 +0300

    'final_dir'

---
+++ README.md
@@ -0,0 +1,18 @@
+# Caesar-cipher-CLI-tool
+
+## Run app
+
+1. Install dependencies
+
+        npm install
+
+2. To run script
+
+        node caesar-cipher-cli -a encode -s 7 -i "./input.txt" -o "./output.txt"
+        node caesar-cipher-cli --action encode --shift 7 --input input.txt --output output.txt
+        node caesar-cipher-cli --action decode --shift 7 --input output.txt --output input.txt
+
+## Description
+
+[Here](https://github.com/rolling-scopes-school/nodejs-course-template/blob/master/TASKS.md)
+
--- caesar-cipher-cli/modules/validator.js
+++ caesar-cipher-cli/modules/validator.js
@@ -13,10 +13,16 @@ const errorHandler = (err) => {
 };
 
 const getAction = () => {
+    if(typeof(args[ACTION[0]]) !== 'string' && typeof(args[ACTION[1]]) !== 'string') {
+        errorHandler(new Error('value of argument -action is missing'));
+    }
     return args[ACTION[0]] || args[ACTION[1]];
 };
 
 const getShift = () => {
+    if(typeof(args[SHIFT[0]]) !== 'number' && typeof(args[SHIFT[1]]) !== 'number') {
+        errorHandler(new Error('value of argument -shift is missing'));
+    }
     return args[SHIFT[0]] || args[SHIFT[1]];
 };
 
@@ -38,11 +44,29 @@ const validateArgs = () => {
     const fs = require('fs');
     let inputFilePath = getInputFile();
     if(inputFilePath) {
-        fs.access(inputFilePath, fs.constants.R_OK, err => errorHandler(err));
+        fs.access(inputFilePath, fs.constants.F_OK, err => {
+            if(err) {
+                errorHandler(new Error('does not exist input file'));
+            }
+        });
+        fs.access(inputFilePath, fs.constants.R_OK, err => {
+            if(err) {
+                errorHandler(new Error('input file do not readable'));
+            }
+        });
     }
     let outputFilePath = getOutputFile();
     if(outputFilePath) {
-        fs.access(outputFilePath, fs.constants.W_OK, err => errorHandler(err));
+        fs.access(outputFilePath, fs.constants.F_OK, err => {
+            if(err) {
+                errorHandler(new Error('does not exist output file'));
+            }
+        });
+        fs.access(outputFilePath, fs.constants.W_OK, err => {
+            if(err) {
+                errorHandler(new Error('output file do not readable'));
+            }
+        });
     }
 };
 
